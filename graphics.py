debc71455043bc3ef8a9309710e37c7f4498b0fa
lucasgandac <lucasgandac@hotmail.com>
2020-09-06 01:37:49 -0300

    Adicionando nos manualmente ao grafico

--- graphics.py
+++ graphics.py
@@ -1,5 +1,31 @@
+import plotly.graph_objects as go
 import plotly.express as px
 
-fig = px.line(x=["a","b","c"], y=[1,3,2], title="sample figure")
-print(fig)
+
+node_trace = go.Scatter(
+        x=[10,15, 20], y=[10,15, 20],
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            reversescale=True,
+            size=20,
+            line_width=2))
+          
+edge_trace = go.Scatter(
+        x=[10,15], y=[10,15],
+        line=dict(width=1, color='magenta'),
+        hoverinfo='none',
+        mode='lines')
+
+
+
+fig = go.Figure(data=[node_trace, edge_trace],layout=go.Layout(
+   title='<b>'+"Artistas Relacionados"+'</b>',
+     titlefont_size=16,
+    margin=dict(b=20, l=5, r=5, t=40), 
+
+    
+))
+
+
 fig.show()
