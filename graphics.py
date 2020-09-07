dfd3e84ec248e6f0edc829b296f3035234982943
lucasgandac <lucasgandac@hotmail.com>
2020-09-07 14:43:23 -0300

    Adicionando no por camada

--- graphics.py
+++ graphics.py
@@ -1,25 +1,41 @@
 import plotly.graph_objects as go
 import plotly.express as px
+import array as arr
 
-
-node_trace = go.Scatter(
-        x=[10,15, 20], y=[10,15, 20],
+cars = ["Ford", "Volvo", "BMW"] 
+teste = [1, 2 , 3],
+x_val = arr.array('i', [10,15, 20, 150, 22, 80, 66]),
+y_val = [10,15, 20, 150, 80, 17, 37],
+y_value = [20,30],
+ 
+f_node_trace = go.Scatter(
+        x=[ 250 , 250], y = [250 ,250],
         mode='markers',
         hoverinfo='text',
         marker=dict(
             reversescale=True,
-            size=20,
+            size=25,
+            line_width=2))
+
+s_node_trace = go.Scatter(
+        x=[ 0 , 500], y = [0 ,500],
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            reversescale=True,
+            size=25,
             line_width=2))
           
+f_node_trace.text = cars
+
 edge_trace = go.Scatter(
-        x=[10,15], y=[10,15],
+        x=[10,15], y=[0,5],
         line=dict(width=1, color='magenta'),
         hoverinfo='none',
         mode='lines')
 
 
-
-fig = go.Figure(data=[node_trace, edge_trace],layout=go.Layout(
+fig = go.Figure(data=[f_node_trace, s_node_trace, edge_trace],layout=go.Layout(
    title='<b>'+"Artistas Relacionados"+'</b>',
      titlefont_size=16,
     margin=dict(b=20, l=5, r=5, t=40), 
